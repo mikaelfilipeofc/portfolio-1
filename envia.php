a8a7050aa2c411b738f7f5ca404da73c942971a9
Mikael Filipe <165120574+mikaelfilipeofc@users.noreply.github.com>
2024-04-23 01:58:35 -0300

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $telefone = addcslashes($_POST['telefone']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $para = "mkfsoftwareoficial@gmail.com";
+    $assunto = "Contato - Portfólio";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: thesoftwarehouseoficial@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Erro ao enviar E-mail!");
+    }
+
+?>
